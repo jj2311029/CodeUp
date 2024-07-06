3311be851b572a311bc15443465d466173ec12d0
jj2311029 <jj2311029@naver.com>
2024-07-07 01:54:27 +0900

    00

---
+++ CodeUp/240706-2.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+void print(int k, int i)
+{
+	if (k == i)
+	{
+		printf("*\n");
+		return;
+	}
+	i++;
+	printf("*");
+	return print(k, i);
+}
+
+void f(int n,int k)
+{
+	if (n == k)
+	{
+		//print(k, 0);
+		return;
+	}
+	print(k, 0);
+	return f(n, k + 1);
+}
+
+
+
+
+
+int main()
+{
+	int n;
+	cin >> n;
+	f(n, 0);
+}
